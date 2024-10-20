c8d75377df9f3d6b6a2b085df928699d6b9d8164
babyloos <babyloos2501@gmail.com>
2024-10-20 16:07:20 +0900

    wip ゲーム画面作成

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,60 +1,65 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default function HomeScreen() {
+type ItemProps = {title: string};
+
+const Tile = ({title}: ItemProps) => {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+      <Text>{title}</Text>
+    </View>
   );
 }
 
+const DATA = [
+  {
+    title: '1'
+  },
+  {
+    title: '2'
+  },
+  {
+    title: '3'
+  },
+]
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>ステージ1</Text>
+      </View>
+      {/* <FlatList
+        data={DATA}
+        numColumns={3}
+        renderItem={({item}) => <Tile title={item.title} />}
+        keyExtractor={item => item.title}
+      /> */}
+    </SafeAreaView>
+    );
+}
+
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#334455',
+  },
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    marginTop: 32,
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 32,
+    textAlign: 'center',
+    marginHorizontal: 100,
+    fontWeight: '600',
+    color: '#005500',
+    backgroundColor: 'white',
   },
   stepContainer: {
     gap: 8,
