6769ad0deefee4508591ad1eda4bede8f28866ed
babyloos <babyloos2501@gmail.com>
2024-11-03 05:16:04 +0900

    結果画面のスタイルを設定

--- components/ResultPanel.tsx
+++ components/ResultPanel.tsx
@@ -6,11 +6,11 @@ import {
 } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-type ItemProps = { isVisible: boolean, result: number, rePlayCallback: () => void}
+type ItemProps = { isVisible: boolean, result: number, rePlayCallback: () => void }
 const ResultPanel = (props: ItemProps) => {
     const { width, height } = Dimensions.get('window');
     const panelWidth = width;
-    const panelHeight = height / 2;
+    const panelHeight = height / 1.8;
     const top = height / 5;
     const display = props.isVisible ? 'flex' : 'none';
 
@@ -19,33 +19,67 @@ const ResultPanel = (props: ItemProps) => {
             display: display,
             position: 'absolute',
             top: top,
-            width: panelWidth,
+            left: 12,
+            width: panelWidth - 27,
             height: panelHeight,
-            backgroundColor: 'limegreen',
+            backgroundColor: '#52B6DE',
+            borderRadius: 6,
+            alignItems: 'center',
+        },
+        titleContainer: {
+            marginTop: 48,
+            height: 84,
+            width: 256,
+            backgroundColor: '#F58B44',
+            borderRadius: 6,
         },
         title: {
             marginTop: 10,
             fontSize: 50,
+            fontWeight: '600',
             textAlign: 'center',
+            color: '#003366',
         },
-        contents: {
-            marginTop: 50,
+        retryContainer: {
+            marginTop: 68,
+            height: 72,
+            width: 248,
+            backgroundColor: '#F58B44',
+            borderRadius: 6,
+        },
+        retry: {
+            marginTop: 10,
             fontSize: 50,
             textAlign: 'center',
+            fontWeight: '400',
+            color: '#4B5161',
         },
-        restartButton: {
-            backgroundColor: 'red',
-            marginStart: 20,
-            marginEnd: 20,
+        nextContainer: {
+            marginTop: 48,
+            height: 72,
+            width: 178,
+            backgroundColor: '#F58B44',
+            borderRadius: 6,
+        },
+        next: {
+            marginTop: 10,
+            fontSize: 50,
+            textAlign: 'center',
+            fontWeight: '400',
+            color: '#4B5161',
         }
     });
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>結果</Text>
-            <Text style={styles.contents}>正解数: {props.result} / 9</Text>
-            <TouchableOpacity onPress={props.rePlayCallback}>
-                <Text style={[styles.contents, styles.restartButton]}>再プレイ</Text>
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>合格</Text>
+            </View>
+            <View style={styles.retryContainer}>
+                <Text style={styles.retry}>もう一度</Text>
+            </View>
+            <TouchableOpacity style={styles.nextContainer} onPress={props.rePlayCallback}>
+                <Text style={[styles.retry, styles.next]}>次へ</Text>
             </TouchableOpacity>
         </View>
     );
