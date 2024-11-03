c36c9aa4e44d53a1914109bc95caef768121aa1f
babyloos <babyloos2501@gmail.com>
2024-11-03 16:34:14 +0900

    カラーパレットを追加

---
+++ app/util/constants.tsx
@@ -0,0 +1,14 @@
+const colors = {
+    base: '#F58B44',
+    baseShadow: '#D2691E',
+    background: '#FDF5EB',
+    text: '#4B5161',
+    disabled: '#70737C',
+    disabledShadow: '#4E5159',
+    panel: '#52B6DE',
+    panelShadow: '#2E8EB8',
+    orange: '#F06E1D',
+    resultTitle: '#003366',
+}
+
+export default colors;
