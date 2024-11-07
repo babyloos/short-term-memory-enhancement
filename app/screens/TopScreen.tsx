a4e369eadb00540e9d8092fec74e90bcccf4e6ab
babyloos <babyloos2501@gmail.com>
2024-11-07 19:36:25 +0900

    トップ画面仮追加

---
+++ app/screens/TopScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+type TopScreenProps = {};
+const TopScreen = ({}: TopScreenProps) => {
+    return (
+        <View>
+            <Text>Top Screen</Text>
+        </View>
+    );
+}
+
+export default TopScreen;
