7e60913c1cfe9325a448046985470391ee77ba21
babyloos <babyloos2501@gmail.com>
2024-11-13 19:04:13 +0900

    不要なimport削除

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
-import { View } from "react-native";
 import TopScreen from "./screens/TopScreen";
-import ManualScreen from "./screens/manualScreen";
 
 export default function Home() {
   return (
