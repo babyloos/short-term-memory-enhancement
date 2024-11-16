5b5dda5c8a76150a6c4c410e130fdf04010d2cc2
babyloos <babyloos2501@gmail.com>
2024-11-17 02:53:21 +0900

    最高難易度表示にクリアしたステージの中で最高の値を表示するよう修正

--- app/screens/SelectLevelScreen.tsx
+++ app/screens/SelectLevelScreen.tsx
@@ -13,12 +13,14 @@ import strage from "../util/gameStrage";
 const ManualScreen = () => {
     const data = new Array<LevelTileProps>();
     const [clearedStage, setClearedStage] = useState([0]);
+    const [maxStage, setMaxStage] = useState(0);
 
     type LevelTileProps = { index: number };
 
     useEffect(() => {
         strage.loadClearedStage().then((clearedStage) => {
             setClearedStage(clearedStage);
+            setMaxStage(Math.max(...clearedStage));
         });
     }, []);
 
@@ -129,7 +131,7 @@ const ManualScreen = () => {
                 <Text style={styles.title}>ステージ選択</Text>
             </View>
             <View style={styles.titleContainer}>
-                <Text style={styles.title}>最高難易度: 24</Text>
+                <Text style={styles.title}>最高難易度: {maxStage}</Text>
             </View>
             <FlatList
                 style={styles.flatList}
