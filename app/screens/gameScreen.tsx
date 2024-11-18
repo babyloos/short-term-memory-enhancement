cbbef250c8478d57ae42ed7b75bf91bc87b9081f
babyloos <babyloos2501@gmail.com>
2024-11-18 18:52:04 +0900

    不要なコードを削除

--- app/screens/gameScreen.tsx
+++ app/screens/gameScreen.tsx
@@ -164,17 +164,6 @@ const HomeScreen = () => {
         )
     };
 
-    const arrayShuffle = (array: Array<number>) => {
-        for (let i = (array.length - 1); 0 < i; i--) {
-            let r = Math.floor(Math.random() * (i + 1));
-            let tmp = array[i];
-            array[i] = array[r];
-            array[r] = tmp;
-        }
-
-        return array;
-    }
-
     const countDownStart = () => {
         setCountDownNum(countStartNum);
         setCountDownIsVisible(true);
