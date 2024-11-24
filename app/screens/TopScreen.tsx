1e2f97c884aea8abed56cbe62b62012efc78c308
babyloos <babyloos2501@gmail.com>
2024-11-24 18:47:23 +0900

    テスト用広告表示の追加

--- app/screens/TopScreen.tsx
+++ app/screens/TopScreen.tsx
@@ -2,14 +2,16 @@ import MenuPanel from "@/components/MenuPanel";
 import {
     Button,
     Dimensions,
+    Platform,
     StyleSheet,
     Text,
     View,
 } from "react-native";
 import colors from "../util/constants";
-import { useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useState } from "react";
 import strage from "../util/gameStrage";
 import SoundManager from "../util/soundManager";
+import mobileAds, { TestIds, useInterstitialAd } from 'react-native-google-mobile-ads';
 
 type TopScreenProps = {};
 const TopScreen = ({ }: TopScreenProps) => {
@@ -20,12 +22,55 @@ const TopScreen = ({ }: TopScreenProps) => {
 
     const [nextStage, setNextStage] = useState(1);
 
+    const [unitId, setUnitId] = useState<string>(TestIds.INTERSTITIAL);
+    const { isLoaded, isClosed, load, show } = useInterstitialAd(unitId, {
+        requestNonPersonalizedAdsOnly: false,
+    });
+
     useEffect(() => {
         SoundManager.getInstance().playSound('enterButton');
         strage.loadNextStage().then((nextStage) => {
             setNextStage(nextStage);
         });
+
+        const testUnitID = TestIds.INTERSTITIAL;
+
+        // 実際に広告配信する際のID
+        // 広告ユニットを作成した際に表示されたものを設定する
+        const adUnitID = Platform.select({
+            ios: "ca-app-pub-1479927029413242/9027896134",
+            android: "ca-app-pub-xxxxxxxxxxxxxxxx/yyyyyyyyyy",
+        });
+
+        if (testUnitID) {
+            setUnitId(testUnitID);
+        } else if (adUnitID) {
+            setUnitId(adUnitID);
+        }
     }, []);
+
+    useEffect(() => {
+        if (load) {
+            // 広告をロードする
+            load();
+        }
+    }, [load]);
+
+    useEffect(() => {
+        // 閉じられたら次の広告をロードしておく
+        if (isClosed) {
+            load();
+        }
+    }, [isClosed]);
+
+    const viewInterstitial = useCallback(async () => {
+        // 広告の表示
+        if (isLoaded) {
+            show();
+        } else {
+            console.log("not loaded:", isLoaded);
+        }
+    }, [isLoaded]);
 
     const styles = StyleSheet.create({
         container: {
@@ -63,10 +108,10 @@ const TopScreen = ({ }: TopScreenProps) => {
                 <Text style={styles.title}>メモリータップ</Text>
             </View>
             <View>
-                <Button title="show add" onPress={() => {console.log("show add")}}></Button>
+                <Button title="show add" onPress={() => { viewInterstitial() }}></Button>
             </View>
             <MenuPanel title={"はじめから"} pathname={"/screens/gameScreen"} params={{ stageNum: 1 }} />
-            <MenuPanel title={"続きから"} pathname={"/screens/gameScreen"} params={{ stageNum: nextStage}} />
+            <MenuPanel title={"続きから"} pathname={"/screens/gameScreen"} params={{ stageNum: nextStage }} />
             <MenuPanel title={"ステージ選択"} pathname={"/screens/SelectLevelScreen"} params={{}} />
             <MenuPanel title={"あそび方"} pathname={"/screens/manualScreen"} params={{}} />
             <Text style={styles.version}>ver.1.0.0</Text>
