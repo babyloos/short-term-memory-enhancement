53ef8aa5a65364dd791b4a57533cdb884bb930c5
babyloos <babyloos2501@gmail.com>
2024-11-26 16:58:22 +0900

    android用の本番広告表示設定

--- app/util/admob.tsx
+++ app/util/admob.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useCallback, useEffect, useState } from "react";
+import { Platform } from "react-native";
 import { TestIds, useInterstitialAd } from "react-native-google-mobile-ads";
 
 interface HiddenComponentProps {
@@ -8,17 +9,20 @@ interface HiddenComponentProps {
 
 const Admob: React.FC<HiddenComponentProps> = ({ triggerAction }) => {
     const [iosUnitId, setIosUnitId] = useState<string>('ca-app-pub-1479927029413242/9027896134');
-    const [androidUnitId, setAndroidUnitId] = useState<string>('');
-    const unitId = __DEV__ ? TestIds.INTERSTITIAL : iosUnitId;
-    const { isLoaded, isClosed, load, show } = useInterstitialAd(unitId, {
+    const [androidUnitId, setAndroidUnitId] = useState<string>('ca-app-pub-1479927029413242/9524388451');
+    const unitId = __DEV__ ? TestIds.INTERSTITIAL : Platform.select({
+            ios: iosUnitId,
+            android: androidUnitId,
+        });
+    const { error, isLoaded, isClosed, load, show } = useInterstitialAd(unitId ? unitId : TestIds.INTERSTITIAL, {
         requestNonPersonalizedAdsOnly: false,
     });
 
-    let testUnitID = null;
-
-    if (__DEV__) {
-        testUnitID = TestIds.INTERSTITIAL;
-    }
+    useEffect(() => {
+        if (error) {
+            console.error("Ad Load Error:", error);
+        }
+    }, [error]);
 
     useEffect(() => {
         if (load) {
